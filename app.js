0c2e0f5c5134e69c5ca359e7c865cee241352324
WadeTheDev <echo1006j@gmail.com>
2022-06-01 17:34:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+   function openNav() {
+    document.getElementById("myNav").style.width = "40%";
+   }
+   
+   
+   function closeNav() {
+     document.getElementById("myNav").style.width = "0%";
+   }
+
+
+
+
+
+  
+   const body = document.querySelectorAll('body');
+
+
+
+
+
+const wipe = document.querySelector('.wipe-transition');
+const TLAnim = new TimelineMax();
+
+function delay(n) {
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n)
+  })
+}
+
+barba.init({
+
+  sync: true,
+  
+  transitions: [
+    {
+      async leave(){
+
+        const done = this.async();
+
+
+        TLAnim.to(wipe, {left: '0%', ease: "power2.out", duration: 0.4});
+        
+        await delay(500);
+        done();
+
+      },
+
+      enter(){
+
+        TLAnim
+        .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
+        .set(wipe, {left: '-100%'})
+
+      }
+
+    }
+  ]
+})
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
